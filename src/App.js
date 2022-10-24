0ffac84507d7d5c84f4d00611fbc0d287a5b93d1
RNG Dung TA <rng.dungta@outlook.com>
2022-10-23 21:34:34 -0700

    sign up page 0.1

--- src/App.js
+++ src/App.js
@@ -1,51 +1,198 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 export default function App() {
+  const [information, setInformation] = useState({
+    name: "",
+    phoneNumber: "",
+    password: "",
+  });
+
+  const [confirm, setConfirm] = useState("");
+
+  const [checked, setChecked] = useState("");
+
+  const [errors, setErrors] = useState(["", "", "", "", ""]);
+
+  const DIGIT_EXPRESSION = /^\d$/;
+
+  const passwordExpression =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+
+  const buttonOnClick = () => {
+    const newList = [...errors];
+
+    if (
+      information.name.length < 8 ||
+      DIGIT_EXPRESSION.test(information.name[0])
+    ) {
+      newList[0] = "Name is not valid.";
+    } else {
+      newList[0] = "";
+    }
+
+    if (information.phoneNumber.length < 12) {
+      newList[1] = "Phone number is not valid.";
+    } else {
+      newList[1] = ".";
+    }
+
+    if (!passwordExpression.test(information.password)) {
+      newList[2] = "Password is not valid.";
+    } else {
+      newList[2] = "";
+    }
+
+    if (confirm !== information.password) {
+      newList[3] = "Repeat password is not right.";
+    } else {
+      newList[3] = "";
+    }
+
+    if (checked !== "checked") {
+      newList[4] = "Agree to Terms and Conditions.";
+    } else {
+      newList[4] = "";
+    }
+
+    setErrors(newList);
+    console.log("errors: ", newList);
+  };
+
   return (
-    <div className='container'>
-      <div className='sign-up'>
-        <div className='form'>
+    <div className="container">
+      <div className="sign-up">
+        <div className="form">
           <h1>Sign up</h1>
 
           <form>
             <i class="fa fa-user"></i>
 
-            <input type='text' id='name' name='name' placeholder='Your Name' />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Your Name"
+              maxLength="16"
+              value={information.name}
+              onChange={(event) => {
+                setInformation({
+                  ...information,
+                  name: event.target.value.replace(
+                    /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/,
+                    ""
+                  ),
+                });
+              }}
+            />
 
             <br />
 
             <i class="fa fa-phone"></i>
 
-            <input type='text' id='phone-number' name='phone-number' placeholder='Your Phone Number' />
+            <input
+              type="text"
+              id="phone-number"
+              name="phone-number"
+              placeholder="Your Phone Number"
+              maxLength="12"
+              value={information.phoneNumber}
+              onChange={(event) => {
+                let value = event.target.value;
+
+                if (value.length >= 7) {
+                  setInformation({
+                    ...information,
+                    phoneNumber:
+                      value.replace(/\D/g, "").substring(0, 3) +
+                      "." +
+                      value.replace(/\D/g, "").substring(3, 6) +
+                      "." +
+                      value.replace(/\D/g, "").substring(6),
+                  });
+                } else if (value.length >= 3) {
+                  setInformation({
+                    ...information,
+                    phoneNumber:
+                      value.replace(/\D/g, "").substring(0, 3) +
+                      "." +
+                      value.replace(/\D/g, "").substring(3),
+                  });
+                } else {
+                  setInformation({
+                    ...information,
+                    phoneNumber: value.replace(/\D/g, ""),
+                  });
+                }
+              }}
+            />
 
             <br />
 
             <i class="fa fa-lock"></i>
 
-            <input type='password' id='password' name='password' placeholder='Password' />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              maxLength="16"
+              value={information.password}
+              onChange={(event) =>
+                setInformation({
+                  ...information,
+                  password: event.target.value,
+                })
+              }
+            />
 
             <br />
 
             <i class="fa fa-lock-open"></i>
 
-            <input type='password' id='repeat-password' name='repeat-password' placeholder='Repeat your password' />
+            <input
+              type="password"
+              id="repeat-password"
+              name="repeat-password"
+              placeholder="Repeat your password"
+              maxLength="16"
+              value={confirm}
+              onChange={(event) => {
+                setConfirm(event.target.value);
+              }}
+            />
 
             <br />
 
-            <div className='agreement'>
-              <input type='checkbox' id='agreement' name='agreement' />
+            <div className="agreement">
+              <input
+                type="checkbox"
+                id="agreement"
+                name="agreement"
+                value="checked"
+                onClick={(event) => setChecked(event.target.value)}
+              />
 
-              <label htmlFor='agreement'>I agree all statements in <a href='#'>Terms of service</a></label>
+              <label htmlFor="agreement">
+                I agree all statements in <a href="#">Terms of service</a>
+              </label>
             </div>
 
-            <button typeof='button'>Register</button>
+            <button type="button" onClick={buttonOnClick}>
+              Register
+            </button>
           </form>
         </div>
 
-        <div className='member'>
-          <img src={'https://colorlib.com/etc/regform/colorlib-regform-7/images/signup-image.jpg'} alt='Sign up' />
+        <div className="member">
+          <img
+            src={
+              "https://colorlib.com/etc/regform/colorlib-regform-7/images/signup-image.jpg"
+            }
+            alt="Sign up"
+          />
 
-          <button type='button'>I am already member</button>
+          <button type="button">I am already member</button>
         </div>
       </div>
     </div>
